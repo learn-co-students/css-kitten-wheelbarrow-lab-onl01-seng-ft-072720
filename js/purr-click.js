25e220084e4e5f30d821f9a4cfda93c1203e8caa
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-17 10:21:29 -0500

    Done.

--- js/purr-click.js
+++ js/purr-click.js
@@ -5,4 +5,18 @@ $(function(){
   });
 });
 
+// const yo = document.querySelector("#yo");
+
+// document.querySelector("#yo").addEventListener('mouseover', e => (console.log(e)))
+
+// document.querySelector("body > div > img.kitty-1").addEventListener('click', e => {
+//   const x = e.screenX;
+//   const y = e.screenY;
+//   // debugger;
+//   e.target.style.left = "300px"
+//   setInterval(function() {
+//     e.target.style.left = `${x/2}px`;
+//   },1000)
+// })
+
 
